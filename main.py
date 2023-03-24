e795699f7561420b741af17ebfd3dcec1f087f87
dongzen <krtdj@naver.com>
2023-03-24 11:28:52 +0900

    test alter

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+    print('dj')
 
 
 
